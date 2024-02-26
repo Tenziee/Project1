447fce51c73d4715032e9785a5606417c2852003
L.Teena Xavier <155170860+Tenziee@users.noreply.github.com>
2024-02-26 12:03:41 +0530

    Update multi.c

--- multi.c
+++ multi.c
@@ -5,13 +5,4 @@ int main()
     
     printf("Enter the first number: ");
     scanf("%d", &num1);
-    
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
-    
-    d= num1 * num2;
-    
-    printf("The sum of %d and %d is %d", num1, num2, sum);
-    
-    return 1;
-}
+   
